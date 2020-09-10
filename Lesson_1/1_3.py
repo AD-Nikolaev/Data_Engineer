24905af8c1b8e2b4fdf9ff6f47f0f0df29cca5ee
AD_Nikolaev <dancer.alexsey@gmail.com>
2020-09-10 08:41:35 +0300

    Changed 1_2, 1_3 and 1_5

--- Lesson_1/1_3.py
+++ Lesson_1/1_3.py
@@ -1,3 +1,3 @@
 number = input('Введите однозначное число: ')
 answer = int(number) + int(number + number) + int(number + number + number)
-print(answer)
+print(f'{number} + {number}{number} + {number}{number}{number} = {answer}')
